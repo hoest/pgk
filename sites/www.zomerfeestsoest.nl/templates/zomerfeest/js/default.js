8489a26ff4315eafcbb86b85b44814b5a7a51b9a
Jelle de Jong <jelle@hoest.nl>
2012-01-21 14:05:51 +0100

    Created Zomerfeest theme

---
+++ sites/www.zomerfeestsoest.nl/templates/zomerfeest/js/default.js
@@ -0,0 +1,20 @@
+var preloadArr = new Array();
+var i;
+var currImg = 1;
+
+$(function() {
+  /* preload images */
+  for(i=0; i < imgArr.length; i++){
+    preloadArr[i] = new Image();
+    preloadArr[i].src = imgArr[i];
+  }
+
+  var intID = setInterval(changeImg, 6000);
+});
+
+/* image rotator */
+function changeImg() {
+  $('#header').animate({opacity: 1}, 1000, function() {
+    $(this).css('background-image', 'url(' + preloadArr[currImg++%preloadArr.length].src +')');
+  }).animate({opacity: 1}, 1000);
+}
